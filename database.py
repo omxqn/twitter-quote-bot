0a41edf6ea5b0d2d54b2de98fa7ecf725d2eecbe
Azzam <48726420+omxqn@users.noreply.github.com>
2021-08-06 00:36:09 +0400

    Update database.py

--- database.py
+++ database.py
@@ -5,6 +5,7 @@ global times
 global date
 date = datetime.datetime.now().date()
 times = datetime.datetime.now().time().strftime(f'%H:%M:%S')
+
 db = mysql.connector.connect(
     host='remotemysql.com',
     user='NdWVc90z4c',
@@ -21,20 +22,27 @@ for i in my_curser:
     print(i)
 '''
 
-def new_database(name):
-    # UNSIGNED no - or +
-    my_curser.execute(f'CREATE TABLE {name} (mention_ID int PRIMARY KEY AUTO_INCREMENT,name VARCHAR(50),id int(20) UNSIGNED,date VARCHAR(50),message VARCHAR(50))')
-    return "Completed"
-
-def add_new(name,id,message): # Insert new details
+def new_table(table_name,instractions):
+    '''Create new table'''
     try:
-        my_curser.execute(f'INSERT INTO id_list (name,id,date,message) VALUES {(name, id, str(date)+" "+str(times), message)}')
+        # UNSIGNED there is no - or +
+        # Example: instractions =  mention_ID int PRIMARY KEY AUTO_INCREMENT,name VARCHAR(50),id int(20) UNSIGNED,date VARCHAR(50),message VARCHAR(50)
+        my_curser.execute(f'CREATE TABLE {table_name} ({instractions})')
+        return "Table has been created successfully"
+    except Exception:
+        return 'This table is already existed'
+def add_new(table,name,id,message): # Insert new details
+    '''Add new data in elements'''
+    # table = id_list
+    try:
+        my_curser.execute(f'INSERT INTO {table} (name,id,date,message) VALUES {(name, id, str(date)+" "+str(times), message)}')
         print("committing")
         db.commit()
         print("Data saved successfully in database")
     except Exception:
         try:
-            my_curser.execute(f'INSERT INTO id_list (name,id,date) VALUES {(name, id, str(date) + " " + str(times))}')
+            print("Message has error")
+            my_curser.execute(f'INSERT INTO {table} (name,id,date) VALUES {(name, id, str(date) + " " + str(times))}')
             print("committing")
             db.commit()
             print("Data saved successfully in database")
@@ -42,26 +50,52 @@ def add_new(name,id,message): # Insert new details
             print("Error while adding to database")
 
 
-def get_table_info(table):
+def get_table_info(table,id):
+    '''Get infos of the current table'''
     try:
         my_curser.execute(f'SELECT * FROM {table}')
-        x = []
-        for i in my_curser:
-            x.append(i[2])
-        print("Data imported successfully")
-        return x
+        if id == True:
+            x = []
+            for i in my_curser:
+                x.append(i[2])
+            print("Data imported successfully")
+            return x
+        else:
+            x = []
+            for i in my_curser:
+                x.append(i)
+            print("Data imported successfully")
+            return x
+
     except Exception:
         print("Error while gathering information from database")
 
+def table_columns(table):
+    data = ""
+    my_curser.execute(f"SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'{table}'")
 
-if __name__=="__main__":
+    for i in my_curser:
+        data += ' ' + i[3]
+    data = data.split(" ")
+    data.pop(0)
+
+    return data
+
+
+
+
+if __name__ == "__main__":
     '''in1 = input('Name: ')
     in2 = input('id: ')
     in3 = input('Message: ')'''
-    #print(new_database('id_list'))
+    #print(new_table('vid','mention_ID int PRIMARY KEY AUTO_INCREMENT,name VARCHAR(50),id int(20) UNSIGNED,date VARCHAR(50),message VARCHAR(50)'))
 
-    #add_new(name='Null',id=15115511515,message='Previous messages')
+    add_new(table='vid',name='Non',id=1511515,message='Test_message')
     print('Done... ')
-    print(get_table_info('id_list'))
+    print(get_table_info('vid',id=False))
+    #table_columns('vid')
+
+
+
 
 
